b00e47290746280ccb1657cb362a42741372547e
Eduardo Miani Ferreira <eduardomianiferreira@gmail.com>
2019-02-08 00:19:42 -0200

    Adding more logs

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ var (
 
 // Provides the page application on port 8080
 func main() {
-	http.HandleFunc("/", CepHandler)
+	http.HandleFunc("/", ZipCodeHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -24,13 +24,15 @@ func renderPage(w http.ResponseWriter, data map[string]interface{}) error {
 	return t.Execute(w, data)
 }
 
-// CepHandler is the main handler of application
+// ZipCodeHandler is the main handler of application
 // When it is access with GET, renders the search zipcode page.
 // When it is access with POST, gets the address from Digipix api and shows the result
-func CepHandler(w http.ResponseWriter, r *http.Request) {
+func ZipCodeHandler(w http.ResponseWriter, r *http.Request) {
+	logServer("Incoming request...")
 	switch r.Method {
 	case "GET":
 		// GET: renders the page
+		logServer("Render page...")
 		if err := renderPage(w, nil); err != nil {
 			log.Printf("%v", err)
 			http.Error(w, "Erro ao exibir a página", http.StatusInternalServerError)
@@ -38,26 +40,31 @@ func CepHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	case "POST":
 		zipParam := r.FormValue("zipcode")
+		logServer("ZipCode received: %s", zipParam)
 		pageData := make(map[string]interface{}, 0)
 		pageData["Zipcode"] = zipParam
 		// POST: search the address data given a zipcode
 		zip := onlyNumber.ReplaceAllString(zipParam, "")
 		if len(zip) != 8 {
+			logServer("Invalid ZipCode")
 			pageData["Error"] = "CEP inválido"
 			renderPage(w, pageData)
 			return
 		}
 		addr, err := GetAddress(zip)
 		if err != nil {
+			logServer("Unable to get address: %s", err.Error())
 			pageData["Error"] = "Ocorreu um erro. Por favor tente novamente"
 			renderPage(w, pageData)
 			return
 		}
 		if addr == nil {
+			logServer("Address not found")
 			pageData["Error"] = "Endereço não encontrado"
 			renderPage(w, pageData)
 			return
 		}
+		logServer("Address found!")
 		pageData["Address"] = *addr
 		renderPage(w, pageData)
 	default:
